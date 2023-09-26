368d39bbd3a0d04bf4fb59b957093a10b993c5c5
KIlnur <kilnur1988@rambler.ru>
2023-09-26 10:52:35 +0500

    Регистрирует команду ImportCommand и наводит небольшую красоту с chalk

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -8,14 +8,14 @@ export class HelpCommand implements Command {
 
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
-    ${chalk.bgBlue('Программа для подготовки данных для REST API сервера.')}
+    ${chalk.blue('Программа для подготовки данных для REST API сервера.')}
     Пример:
         ${chalk.bold('cli.js --<command> [--arguments]')}
     Команды:
         ${chalk.bold('--version:')}                   # выводит номер версии
         ${chalk.bold('--help:')}                      # печатает этот текст
-        ${chalk.bold('--import')} ${chalk.green('<path>:')}             # импортирует данные из TSV
-        ${chalk.bold('--generate')} ${chalk.red('<n>')} ${chalk.green('<path>:')} ${chalk.magenta('<url>')}  # генерирует произвольное количество тестовых данных
+        ${chalk.bold('--import')} ${chalk.italic(chalk.green('<path>:'))}             # импортирует данные из TSV
+        ${chalk.bold('--generate')} ${chalk.bold(chalk.red('<n>'))} ${chalk.italic(chalk.green('<path>:'))} ${chalk.bold(chalk.magenta('<url>'))}  # генерирует произвольное количество тестовых данных
     `);
   }
 }
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,3 +2,4 @@ export * from './cli-applications.js';
 export * from './command-parser.js';
 export * from './commands/help.command.js';
 export * from './commands/version.command.js';
+export * from './commands/import.command.js';
