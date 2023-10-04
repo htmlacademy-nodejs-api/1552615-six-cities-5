ca65e49d10f30811273d00d8abfc02b166766db4
KIlnur <kilnur1988@rambler.ru>
2023-10-04 11:21:43 +0500

    Установит пакет dotenv. Добавит поддержку dotenv в RestConfig

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,9 +1,21 @@
+import { DotenvParseOutput, config } from 'dotenv';
 import { Config } from './config.interface.js';
+import { Logger } from '../logger/index.js';
 
 export class RestConfig implements Config {
+  private readonly config: NodeJS.ProcessEnv;
   constructor(
-    private readonly config: NodeJS.ProcessEnv
-  ) {}
+    private readonly logger: Logger
+  ) {
+    const parseOutput = config();
+
+    if (parseOutput.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
+    }
+
+    this.config = <DotenvParseOutput>parseOutput.parsed;
+    this.logger.info('.env file found and successfully parsed!');
+  }
 
   public get(key: string): string | undefined {
     return this.config[key];
